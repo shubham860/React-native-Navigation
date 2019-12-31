589716f5ddb2aaee5c95b44fcac861dcf5db4824
shubham860 <shubham.chauhan860@gmail.com>
2019-12-31 18:15:32 +0530

    commit-1

---
+++ Navigation/components/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {View,Text} from 'react-native';
+
+class Home extends Component {
+    render() {
+        return (
+            <View>
+                <Text style={{fontSize : 28,textAlign : 'center',justifyContent : 'center'}}>Home</Text>
+            </View>
+    )
+        ;
+    }
+}
+
+export default Home;
---
+++ Navigation/components/Test.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {View,Text} from 'react-native';
+
+class Test extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Test</Text>
+            </View>
+        )
+            ;
+    }
+}
+
+export default Test;
