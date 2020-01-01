6bec8c67349d897fcd5c0ffec3da76b73125a68e
shubham860 <shubham.chauhan860@gmail.com>
2020-01-01 22:18:36 +0530

    commit-3

--- Navigation/App.js
+++ Navigation/App.js
@@ -3,25 +3,47 @@ import Home from "./components/Home";
 import Test from './components/Test';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator} from "react-navigation-tabs";
+import {View} from 'react-native'
 import 'react-native-gesture-handler';
 
- const routes = createStackNavigator(
-        {
-            home : Home,
-            test : Test,
+const routes = createStackNavigator(
+    {
+        home : Home,
+        test : Test,
+    },
+    {
+        initialRouteName : 'home',
+        defaultNavigationOptions : {
+            headerStyle: {
+                backgroundColor: 'black',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            },
         },
-        {
-            initialRouteName : 'home',
-        }
- );
+    },
+);
+
+const bottom = createBottomTabNavigator(
+    {
+        home : Home,
+        test : Test
+    }
+)
 
 const AppContainer = createAppContainer(routes);
+const AappContainer = createAppContainer(bottom);
 
 class App extends Component {
     render() {
         return (
-            <AppContainer/>
-     );
+
+                <AppContainer/>
+
+
+     )
     }
 }
 
